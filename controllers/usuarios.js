0c7fcfba02564d2d576a88e4eedfdbfd54711a8d
Violeta1709 <l18070011@huetamo.tecnm.mx>
2023-01-06 12:07:38 -0600

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+const { response, request} = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+const query = req.query;
+
+
+    res.json({
+        msg:'get API -controlador', query
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+
+const body = req.body;
+
+    res.json({
+        msg:'Post API -controlador', body
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+    res.json({
+        msg:'Put API -controlador', id
+    })
+}
+
+
+const usuariosPatch = (req, res = response) => {
+
+    res.json({
+        msg:'Patch API -controlador'
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+
+    res.json({
+        msg:'Delete API -controlador'
+    })
+}
+
+
+module.exports = {
+    usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete
+}
+
